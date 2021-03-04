96a3b0043f9332ef8f826d7251b4871ad73ab21b
DaMinaup6 <danaup6@gmail.com>
2021-03-05 02:39:50 +0800

    algorithm: add rotate image problem

---
+++ leet_code/48_rotate_image.py
@@ -0,0 +1,25 @@
+# -----------------------------------------
+# My Solution
+# -----------------------------------------
+class Solution:
+    def rotate(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        l = 0
+        while n > 1:
+            # rotate outside matrix
+            for idx in range(0, n - 1):
+                matrix[                l][          idx + l], \
+                matrix[          idx + l][(n - 1)       + l], \
+                matrix[(n - 1) - idx + l][                l], \
+                matrix[(n - 1)       + l][(n - 1) - idx + l]  \
+                = \
+                matrix[(n - 1) - idx + l][                l], \
+                matrix[                l][          idx + l], \
+                matrix[(n - 1)       + l][(n - 1) - idx + l], \
+                matrix[          idx + l][(n - 1)       + l]
+
+            n -= 2 # reduce matrix to inner matrix, for example there is a 3 x 3 matrix inside 5 x 5 matrix so need minus 2
+            l += 1 # for 3 x 3 matrix inside 5 x 5 matrix, its position need to add 1, e.g. [0, 0] -> [1, 1]
