0dd393a2be2e423434f5a75bc7a94a26abef250e
DaMinaup6 <danaup6@gmail.com>
2021-03-09 00:19:38 +0800

    algorithm: add enhanced solution for minimum window substring problem

--- leet_code/76_minimum_window_substring.py
+++ leet_code/76_minimum_window_substring.py
@@ -5,36 +5,79 @@
 # Space Complexity: O(m + n)
 # -----------------------------------------
 # m := len(s), n := len(t)
+# from collections import defaultdict
+#
+# def window_valid(s_count, t_count):
+#     for char in t_count:
+#         if t_count[char] > s_count[char]:
+#             return False
+#
+#     return True
+#
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         t_count = defaultdict(int)
+#         for char in t:
+#             t_count[char] += 1
+#
+#         s_count = defaultdict(int)
+#         s_count[s[0]] = 1
+#         left_cursor   = 0
+#         right_cursor  = 0
+#         sub_s_indexes = [-1, -1]
+#         while left_cursor <= right_cursor and right_cursor < len(s):
+#             if window_valid(s_count, t_count):
+#                 if sub_s_indexes == [-1, -1] or sub_s_indexes[1] - sub_s_indexes[0] + 1 > right_cursor - left_cursor + 1:
+#                     sub_s_indexes = [left_cursor, right_cursor]
+#
+#                 s_count[s[left_cursor]] -= 1
+#                 left_cursor += 1
+#             else:
+#                 right_cursor += 1
+#                 if right_cursor < len(s):
+#                     s_count[s[right_cursor]] += 1
+#
+#         return s[sub_s_indexes[0]:(sub_s_indexes[1] + 1)]
+
+# -----------------------------------------
+# My Solution (enhanced)
+#
+# Time  Complexity: O(m + n)
+# Space Complexity: O(m + n)
+# -----------------------------------------
+# m := len(s), n := len(t)
 from collections import defaultdict
-
-def window_valid(s_count, t_count):
-    for char in t_count:
-        if t_count[char] > s_count[char]:
-            return False
-
-    return True
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         t_count = defaultdict(int)
         for char in t:
             t_count[char] += 1
+        uniq_char_num = len(t_count)
 
         s_count = defaultdict(int)
         s_count[s[0]] = 1
+        char_full_num = 1 if s[0] in t_count and 1 >= t_count[s[0]] else 0
+
         left_cursor   = 0
         right_cursor  = 0
         sub_s_indexes = [-1, -1]
         while left_cursor <= right_cursor and right_cursor < len(s):
-            if window_valid(s_count, t_count):
+            if char_full_num == uniq_char_num:
                 if sub_s_indexes == [-1, -1] or sub_s_indexes[1] - sub_s_indexes[0] + 1 > right_cursor - left_cursor + 1:
                     sub_s_indexes = [left_cursor, right_cursor]
 
                 s_count[s[left_cursor]] -= 1
+                if s[left_cursor] in t_count and s_count[s[left_cursor]] < t_count[s[left_cursor]]:
+                    char_full_num -= 1
                 left_cursor += 1
             else:
                 right_cursor += 1
                 if right_cursor < len(s):
                     s_count[s[right_cursor]] += 1
+                    # must use == instead of >=, for example
+                    # s == "ABBC", t == 'ABC', if use >= then "ABB" will considered as valid window since when "B" of "AB" and last "B" of "ABB" matches >= condition
+                    if s[right_cursor] in t_count and s_count[s[right_cursor]] == t_count[s[right_cursor]]:
+                        char_full_num += 1
 
         return s[sub_s_indexes[0]:(sub_s_indexes[1] + 1)]
