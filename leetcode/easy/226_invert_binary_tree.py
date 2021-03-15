af7c92420c21a55e738b7dec29f4ce6896744778
DaMinaup6 <danaup6@gmail.com>
2021-03-15 19:34:17 +0800

    algorithm: invert binary tree problem

---
+++ leetcode/easy/226_invert_binary_tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val   = val
+        self.left  = left
+        self.right = right
+
+# -----------------------------------------
+# My Solution
+#
+# Time  Complexity: O(n)
+# Space Complexity: O(1)
+# -----------------------------------------
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root is None:
+            return None
+
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
