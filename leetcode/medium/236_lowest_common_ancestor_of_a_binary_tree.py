5395d0567c8467318e24fb4851b15db5c59c5505
DaMinaup6 <danaup6@gmail.com>
2021-03-15 23:35:07 +0800

    algorithm: lowest common ancestor of a binary tree problem

---
+++ leetcode/medium/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val   = x
+        self.left  = None
+        self.right = None
+
+# -----------------------------------------
+# Model Solution
+#
+# Time  Complexity: O(n)
+# Space Complexity: O(1)
+# -----------------------------------------
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None:
+            return None
+        elif p == root or q == root:
+            return root
+
+        left  = self.lowestCommonAncestor(root.left,  p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+
+        return left or right
