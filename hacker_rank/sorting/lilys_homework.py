e2c8f4c41f98277929f90838e7764ff083bfc14f
DaMinaup6 <danaup6@gmail.com>
2021-03-20 17:17:37 +0800

    algorithm: simplify Lilys homework problem

--- hacker_rank/sorting/lilys_homework.py
+++ hacker_rank/sorting/lilys_homework.py
@@ -1,27 +1,24 @@
-import copy
-
 def count_swap(arr, sorted_arr, mapping):
     swap_count = 0
     for idx in range(0, len(arr)):
         if arr[idx] == sorted_arr[idx]:
             continue
         else:
-            arr[idx], arr[mapping[sorted_arr[idx]]] = arr[mapping[sorted_arr[idx]]], arr[idx]
-            mapping[arr[mapping[sorted_arr[idx]]]] = mapping[sorted_arr[idx]]
-            mapping[arr[idx]] = idx
+            sorted_idx = mapping[sorted_arr[idx]]
+            arr[idx], arr[sorted_idx] = arr[sorted_idx], arr[idx]
+            mapping[arr[sorted_idx]], mapping[arr[idx]] = sorted_idx, idx
+
             swap_count += 1
 
     return swap_count
 
 def lilysHomework(arr):
-    arr_len = len(arr)
-    sorted_arr = sorted(arr)
-
     mapping = {}
-    for idx in range(0, arr_len):
+    for idx in range(0, len(arr)):
         mapping[arr[idx]] = idx
 
-    asc_swap_count = count_swap(copy.copy(arr), sorted_arr, copy.copy(mapping))
-    desc_swap_count = count_swap(copy.copy(arr), sorted_arr[::-1], copy.copy(mapping))
+    sorted_arr = sorted(arr)
+    asc_swap_count  = count_swap(arr.copy(), sorted_arr,       mapping.copy())
+    desc_swap_count = count_swap(arr.copy(), sorted_arr[::-1], mapping.copy())
 
     return min(asc_swap_count, desc_swap_count)
