5675443c5ee314d52cf3f86b8317f3a0ec1cfcae
DaMinaup6 <danaup6@gmail.com>
2021-03-20 21:17:57 +0800

    algorithm: add recursion solution for construct binary tree from preorder and inorder traversal

--- leetcode/medium/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ leetcode/medium/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -38,6 +38,25 @@ class Solution:
         return root
 
 # -----------------------------------------
+# Recursion
+#
+# Time  Complexity: O(n^2)
+# Space Complexity: O(n^2)
+# -----------------------------------------
+# Ref: https://blog.csdn.net/fuxuemingzhu/article/details/80775173
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if len(preorder) == 0 or len(inorder) == 0:
+            return None
+
+        root = TreeNode(preorder[0])
+        root_index = inorder.index(preorder[0])
+        root.left  = self.buildTree(preorder[1:(root_index + 1)], inorder[:root_index])
+        root.right = self.buildTree(preorder[(root_index + 1):],  inorder[(root_index + 1):])
+
+        return root
+
+# -----------------------------------------
 # Model Solution
 #
 # Time  Complexity: O(n)
