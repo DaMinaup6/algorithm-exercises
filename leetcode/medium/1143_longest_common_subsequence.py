f2a0b30dd7c575a9eac2aa618c82cf4fb42339f4
DaMinaup6 <danaup6@gmail.com>
2021-03-28 00:36:25 +0800

    algorithm: longest common subsequence problem

---
+++ leetcode/medium/1143_longest_common_subsequence.py
@@ -0,0 +1,18 @@
+# -----------------------------------------
+# Model Solution
+#
+# Time  Complexity: O(mn)
+# Space Complexity: O(mn)
+# -----------------------------------------
+# Ref: https://github.com/changgyhub/leetcode_101/blob/master/LeetCode%20101%20-%20A%20LeetCode%20Grinding%20Guide%20(C%2B%2B%20Version).pdf
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[len(text1)][len(text2)]
