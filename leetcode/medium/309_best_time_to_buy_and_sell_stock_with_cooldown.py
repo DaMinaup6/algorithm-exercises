df80ccf4ff5e33c2289f8cff233f3678e3ba8c1e
DaMinaup6 <danaup6@gmail.com>
2021-03-29 12:59:19 +0800

    algorithm: add at most k transactions solution for best time to buy and sell stock with cooldown problem

--- leetcode/medium/309_best_time_to_buy_and_sell_stock_with_cooldown.py
+++ leetcode/medium/309_best_time_to_buy_and_sell_stock_with_cooldown.py
@@ -44,3 +44,24 @@ class Solution:
             curr_sell = prev_keep + prices[index]
             curr_rest = max(prev_rest, prev_sell)
         return max(curr_sell, curr_rest)
+
+# -----------------------------------------
+# At most k transactions
+# -----------------------------------------
+# Ref: https://www.youtube.com/watch?v=oDhu5uGq_ic
+class Solution:
+    def maxProfit(self, k, prices: List[int]) -> int: # for problem 309 k would be math.ceil(len(prices) / 2)
+        if len(prices) < 2:
+            return 0
+
+        # i := transaction, j := day, dp[i][j] := maximum profit can make in i-th transaction at j-th day
+        dp = [[0] * len(prices) for _ in range(k + 1)]
+        # TODO: enhance time and space complexity
+        for i in range(1, k + 1):
+            for j in range(1, len(prices)):
+                dp[i][j] = max(
+                    dp[i][j - 1],
+                    # just like in problem 188, but when buy at day k, we can only use max profit from k - 2 day since there is a one day cooldown
+                    prices[j] + max(dp[i - 1][k - 2] - prices[k] if k >= 2 else -prices[k] for k in range(j)),
+                )
+        return dp[k][len(prices) - 1]
