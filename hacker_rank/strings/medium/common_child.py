b76bd049ff45abcc1f4a59e81216279c4968273d
DaMinaup6 <danaup6@gmail.com>
2021-04-05 13:17:15 +0800

    algorithm: common child problem

---
+++ hacker_rank/strings/medium/common_child.py
@@ -0,0 +1,41 @@
+# -----------------------------------------
+# Model Solution
+#
+# Time  Complexity: O(mn)
+# Space Complexity: O(mn)
+# -----------------------------------------
+# m := len(s1), n := len(s2)
+# Ref: https://github.com/changgyhub/leetcode_101/blob/master/LeetCode%20101%20-%20A%20LeetCode%20Grinding%20Guide%20(C%2B%2B%20Version).pdf
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the commonChild function below.
+def commonChild(s1, s2):
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[len(s1)][len(s2)]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = commonChild(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
