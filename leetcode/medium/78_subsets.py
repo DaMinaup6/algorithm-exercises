37b3f2ca6d1e40eb34a9497c6e40a49fa76250bf
DaMinaup6 <danaup6@gmail.com>
2021-04-07 23:24:48 +0800

    algorithm: add backtracking solution for subsets problem

--- leetcode/medium/78_subsets.py
+++ leetcode/medium/78_subsets.py
@@ -19,3 +19,26 @@ class Solution:
             generate_subset([], 0, size)
 
         return subsets
+
+# -----------------------------------------
+# Backtracking
+#
+# Time  Complexity: O(2^n)
+# Space Complexity: O(2^n)
+# -----------------------------------------
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        def backtrack(subset_size, start_index, curr_subset):
+            if len(curr_subset) == subset_size:
+                output.append(curr_subset.copy())
+                return
+
+            for index in range(start_index, len(nums)):
+                curr_subset.append(nums[index])
+                backtrack(subset_size, index + 1, curr_subset)
+                curr_subset.pop()
+
+        for subset_size in range(len(nums) + 1):
+            backtrack(subset_size, 0, [])
+        return output
