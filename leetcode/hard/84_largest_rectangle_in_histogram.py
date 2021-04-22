adcd5ebb6b5edaa2159e2c5e4c2bf3ed048b67b4
DaMinaup6 <danaup6@gmail.com>
2021-04-22 23:13:36 +0800

    algorithm: little refactor of largest rectangle in histogram problem

--- leetcode/hard/84_largest_rectangle_in_histogram.py
+++ leetcode/hard/84_largest_rectangle_in_histogram.py
@@ -7,21 +7,17 @@
 # Ref: https://www.youtube.com/watch?v=lsQTYlCiU6c
 class Solution:
     def largestRectangleArea(self, heights):
-        h_stack = [] # stack of heights
-        p_stack = [] # stack of positions
+        hight_start_index_stack = [] # stack of (height, start_index)
         heights.append(0) # force to empty stack before while loop ends
 
         max_area = 0
-        for index in range(len(heights)):
-            last_position = len(heights) + 1
+        for curr_index, curr_height in enumerate(heights):
+            start_index = len(heights) + 1
+            while len(hight_start_index_stack) > 0 and hight_start_index_stack[-1][0] > curr_height:
+                prev_height, start_index = hight_start_index_stack.pop()
+                max_area = max(max_area, (curr_index - start_index) * prev_height)
 
-            while len(h_stack) > 0 and h_stack[-1] > heights[index]:
-                last_position = p_stack[-1]
-                current_area  = (index - p_stack.pop()) * h_stack.pop()
-                max_area = max(max_area, current_area)
-
-            if len(h_stack) == 0 or h_stack[-1] < heights[index]:
-                h_stack.append(heights[index])
-                p_stack.append(min(last_position, index))
+            if len(hight_start_index_stack) == 0 or hight_start_index_stack[-1][0] < curr_height:
+                hight_start_index_stack.append((curr_height, min(start_index, curr_index)))
 
         return max_area
