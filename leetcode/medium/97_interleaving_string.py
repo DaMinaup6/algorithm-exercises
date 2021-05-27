421311598d59eef4f657a4e645999ea7ebdf5297
DaMinaup6 <danaup6@gmail.com>
2021-05-27 23:41:15 +0800

    algorithm: update interleaving string problem

--- leetcode/medium/97_interleaving_string.py
+++ leetcode/medium/97_interleaving_string.py
@@ -54,12 +54,12 @@ class Solution:
                 s1_char = s1[s1_index] if s1_index < len(s1) else ''
                 s2_char = s2[s2_index] if s2_index < len(s2) else ''
                 s3_char = s3[s1_index + s2_index]
-                if s3_char in [s1_char, s2_char]:
-                    if s1_char != s2_char:
-                        dp[s1_index][s2_index] = dp[s1_index + 1][s2_index] if s3_char == s1_char else dp[s1_index][s2_index + 1]
-                    else:
-                        dp[s1_index][s2_index] = dp[s1_index + 1][s2_index] or dp[s1_index][s2_index + 1]
-
+                if s3_char == s1_char and s3_char != s2_char:
+                    dp[s1_index][s2_index] = dp[s1_index + 1][s2_index]
+                elif s3_char != s1_char and s3_char == s2_char:
+                    dp[s1_index][s2_index] = dp[s1_index][s2_index + 1]
+                elif s3_char == s1_char == s2_char:
+                    dp[s1_index][s2_index] = dp[s1_index + 1][s2_index] or dp[s1_index][s2_index + 1]
         return dp[0][0]
 
 # -----> Version 2: Space Complexity: O(n)
@@ -79,11 +79,12 @@ class Solution:
                 s1_char = s1[s1_index] if s1_index < len(s1) else ''
                 s2_char = s2[s2_index] if s2_index < len(s2) else ''
                 s3_char = s3[s1_index + s2_index]
-                if s3_char in [s1_char, s2_char]:
-                    if s1_char != s2_char:
-                        next_dp[s2_index] = curr_dp[s2_index] if s3_char == s1_char else next_dp[s2_index + 1]
-                    else:
-                        next_dp[s2_index] = curr_dp[s2_index] or next_dp[s2_index + 1]
-            curr_dp = next_dp
 
+                if s3_char == s1_char and s3_char != s2_char:
+                    next_dp[s2_index] = curr_dp[s2_index]
+                elif s3_char != s1_char and s3_char == s2_char:
+                    next_dp[s2_index] = next_dp[s2_index + 1]
+                elif s3_char == s1_char == s2_char:
+                    next_dp[s2_index] = curr_dp[s2_index] or next_dp[s2_index + 1]
+            curr_dp = next_dp
         return curr_dp[0]
