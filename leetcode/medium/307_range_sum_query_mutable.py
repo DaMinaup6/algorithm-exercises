a8a74d929452366409ec39481cc1fb69a18e5d73
DaMinaup6 <danaup6@gmail.com>
2021-06-01 22:39:05 +0800

    algorithm: range sum query mutable problem

---
+++ leetcode/medium/307_range_sum_query_mutable.py
@@ -0,0 +1,40 @@
+# -----------------------------------------
+# Model Solution: Binary indexes Tree
+#
+# Space Complexity: O(n)
+# -----------------------------------------
+# Ref: https://www.youtube.com/watch?v=WbafSgetDDk
+class NumArray:
+
+    # -----> Time Complexity: O(n)
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.binary_indexed_tree = [0] * (len(nums) + 1)
+        for index in range(len(nums)):
+            self.update_tree_val(index, nums[index])
+
+    # -----> Time Complexity: O(log(n))
+    def update_tree_val(self, index: int, diff: int) -> None:
+        index += 1
+        while index <= len(self.nums):
+            self.binary_indexed_tree[index] += diff
+            index += index & (-index)
+
+    # -----> Time Complexity: O(log(n))
+    def get_sum(self, index: int) -> int:
+        range_sum = 0
+
+        index += 1
+        while index > 0:
+            range_sum += self.binary_indexed_tree[index]
+            index -= index & (-index)
+        return range_sum
+
+    # -----> Time Complexity: O(log(n))
+    def update(self, index: int, val: int) -> None:
+        self.update_tree_val(index, val - self.nums[index])
+        self.nums[index] = val
+
+    # -----> Time Complexity: O(log(n))
+    def sumRange(self, left: int, right: int) -> int:
+        return self.get_sum(right) - self.get_sum(left - 1)
