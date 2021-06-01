dde6ed6f4e71548382ad68dfafc3f4ac1ba9142d
DaMinaup6 <danaup6@gmail.com>
2021-06-01 20:52:11 +0800

    algorithm: update three sum problem

--- leetcode/medium/15_three_sum.py
+++ leetcode/medium/15_three_sum.py
@@ -1,18 +1,52 @@
-def threeSum(nums):
-    if len(nums) <= 2:
-        return []
+# -----------------------------------------
+# My Solution: Three Pointers
+#
+# Time  Complexity: O(n^2)
+# Space Complexity: O(1)
+# -----------------------------------------
+# Note: If output considered in space complexity then it takes O(n)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
 
-    nums.sort()
-    output = set()
-    for index, num_1 in enumerate(nums[:-2]):
-        if index >= 1 and num_1 == nums[index - 1]:
-            continue
+        output  = set()
+        pointer = 1
+        while pointer <= len(nums) - 2:
+            prev_pointer, next_pointer = pointer - 1, pointer + 1
+            while prev_pointer >= 0 and next_pointer < len(nums):
+                curr_sum = nums[prev_pointer] + nums[pointer] + nums[next_pointer]
+                if curr_sum == 0:
+                    output.add((nums[prev_pointer], nums[pointer], nums[next_pointer]))
+                    prev_pointer -= 1
+                    next_pointer += 1
+                elif curr_sum > 0:
+                    prev_pointer -= 1
+                else:
+                    next_pointer += 1
+            pointer += 1
+        return output
 
-        temp_dict = {}
-        for num_2 in nums[index + 1:]:
-            if num_2 not in temp_dict:
-                temp_dict[-(num_1 + num_2)] = 1
-            else:
-                output.add((num_1, num_2, -(num_1 + num_2)))
+# -----------------------------------------
+# My Solution
+#
+# Time  Complexity: O(n^2)
+# Space Complexity: O(n)
+# -----------------------------------------
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
 
-    return list(output) # NOTE: output is list of tuples
+        output = set()
+        for index, num_1 in enumerate(nums[:-2]):
+            if index >= 1 and num_1 == nums[index - 1]:
+                continue
+
+            temp_dict = set()
+            for num_2 in nums[(index + 1):]:
+                # if num_2 already in temp_dict, then it must be added by other_num such that -(num_1 + other_num) == num_2 => other_num == -(num_1 + num_2)
+                # so other_num exists
+                if num_2 not in temp_dict:
+                    temp_dict.add(-(num_1 + num_2))
+                else:
+                    output.add((num_1, num_2, -(num_1 + num_2)))
+        return output
