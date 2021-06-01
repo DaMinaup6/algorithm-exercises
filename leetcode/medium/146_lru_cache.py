622e1191188b9badd666589f3ff2743346781aaa
DaMinaup6 <danaup6@gmail.com>
2021-06-01 21:22:12 +0800

    algorithm: update LRU cache problem

--- leetcode/medium/146_lru_cache.py
+++ leetcode/medium/146_lru_cache.py
@@ -48,64 +48,54 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
+
         self.hash = {}
         self.head = Node(-1, -1)
         self.tail = Node(-1, -1)
         self.size = 0
 
-        self.head.prev = self.tail
-        self.tail.next = self.head
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
         if self.hash.get(key) is None:
             return -1
 
         node = self.hash[key]
-        self.__isolate_node(node)
-        self.__insert_node_to_first(node)
+        self.insert_node_to_first(node)
 
         return node.value
 
     def put(self, key: int, value: int) -> None:
         if self.get(key) == -1:
             if self.size == self.capacity:
-                self.__remove_last_node()
-            self.__add_new_node(key, value)
+                self.remove_last_node()
+            self.size += 1
+
+            new_node = Node(key, value)
+            self.insert_node_to_first(new_node)
         else:
+            # The node called already updated by self.get so no need self.insert_node_to_first here
             self.hash[key].value = value
 
-    def __isolate_node(self, node: Node) -> None:
-        # (... - prev_node - node - next_node - ...) -> (... - prev_node - next_node - ...)
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        node.prev = None
-        node.next = None
+    def insert_node_to_first(self, node: Node) -> None:
+        # head <-> first_node <-> ... <-> prev_node <-> node <-> next_node <-> ... => head <-> node <-> first_node <-> ... <-> prev_node <-> next_node <-> ...
+        if node.prev is not None and node.next is not None:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next      = node
 
-    def __insert_node_to_first(self, node: Node) -> None:
-        # insert node between first_node and self.head
-        # (... - first_node - self.head) -> (... - first_node - node - self.head)
-        first_node = self.head.prev
-        first_node.next = node
-        self.head.prev  = node
-        node.prev = first_node
-        node.next = self.head
+        self.hash[node.key] = node
 
-    def __remove_last_node(self) -> None:
-        # remove last_node between self.tail and next_node
-        # (self.tail - last_node - next_node - ...) -> (self.tail - next_node - ...)
-        last_node = self.tail.next
-        next_node = last_node.next
-
-        self.tail.next = next_node
-        next_node.prev = self.tail
+    def remove_last_node(self) -> None:
+        # head <-> ... <-> prev_node <-> last_node <-> tail => head <-> ... <-> prev_node <-> tail
+        last_node = self.tail.prev
+        last_node.prev.next = self.tail
+        last_node.next.prev = last_node.prev
 
         del self.hash[last_node.key]
         del last_node
         self.size -= 1
-
-    def __add_new_node(self, key: int, value: int) -> None:
-        new_node = Node(key, value)
-
-        self.__insert_node_to_first(new_node)
-        self.hash[new_node.key] = new_node
-        self.size += 1
