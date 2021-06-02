b4770bdb742679dbe32f89f7cffb7df678ac5706
DaMinaup6 <danaup6@gmail.com>
2021-06-02 17:43:34 +0800

    algorithm: update wildcard matching problem

--- leetcode/hard/44_wildcard_matching.py
+++ leetcode/hard/44_wildcard_matching.py
@@ -1,10 +1,11 @@
 # -----------------------------------------
-# My Solution
+# My Solution: Dynamic Programming
 #
-# Time  Complexity: O(mn)
-# Space Complexity: O(mn)
+# Time Complexity: O(mn)
 # -----------------------------------------
 # m := len(s), n := len(p)
+
+# -----> Version 1: Space Complexity: O(mn)
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
@@ -22,35 +23,25 @@ class Solution:
                     dp[sub_s_len][sub_p_len] = dp[sub_s_len - 1][sub_p_len - 1]
                 elif curr_p_char == '*':
                     dp[sub_s_len][sub_p_len] = dp[sub_s_len][sub_p_len - 1] or dp[sub_s_len - 1][sub_p_len]
+        return dp[-1][-1]
 
-        return dp[len(s)][len(p)]
-
-# -----------------------------------------
-# My Solution: Space Compression
-#
-# Time  Complexity: O(mn)
-# Space Complexity: O(n)
-# -----------------------------------------
-# m := len(s), n := len(p)
+# -----> Version 2: Space Complexity: O(n)
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        dp_prev = [False] * (len(p) + 1)
         dp_curr = [False] * (len(p) + 1)
-        dp_prev[0] = True
+        dp_curr[0] = True
 
         for sub_p_len in range(1, len(p) + 1):
-            dp_prev[sub_p_len] = (p[sub_p_len - 1] == '*') and dp_prev[sub_p_len - 1]
+            dp_curr[sub_p_len] = (p[sub_p_len - 1] == '*') and dp_curr[sub_p_len - 1]
 
         for sub_s_len in range(1, len(s) + 1):
+            dp_next = [False] * (len(p) + 1)
             for sub_p_len in range(1, len(p) + 1):
                 curr_s_char = s[sub_s_len - 1]
                 curr_p_char = p[sub_p_len - 1]
                 if curr_s_char == curr_p_char or curr_p_char == '?':
-                    dp_curr[sub_p_len] = dp_prev[sub_p_len - 1]
+                    dp_next[sub_p_len] = dp_curr[sub_p_len - 1]
                 elif curr_p_char == '*':
-                    dp_curr[sub_p_len] = dp_curr[sub_p_len - 1] or dp_prev[sub_p_len]
-
-            dp_prev = dp_curr
-            dp_curr = [False] * (len(p) + 1)
-
-        return dp_prev[len(p)]
+                    dp_next[sub_p_len] = dp_next[sub_p_len - 1] or dp_curr[sub_p_len]
+            dp_curr = dp_next
+        return dp_curr[-1]
