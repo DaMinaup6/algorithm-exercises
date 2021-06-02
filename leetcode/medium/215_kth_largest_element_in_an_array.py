4540c22a85ae73a40e915ee5e51778803cae284b
DaMinaup6 <danaup6@gmail.com>
2021-06-02 22:34:02 +0800

    algorithm: update kth largest element in an array problem

--- leetcode/medium/215_kth_largest_element_in_an_array.py
+++ leetcode/medium/215_kth_largest_element_in_an_array.py
@@ -42,24 +42,23 @@ class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         left, right = 0, len(nums) - 1
         while left <= right:
-            pivot_idx = left
-            new_pivot_idx = self.partition_around_pivot(left, right, pivot_idx, nums)
+            pivot_idx = self.partition(nums, left, right)
+            if pivot_idx == len(nums) - k:
+                return nums[pivot_idx]
+            elif pivot_idx > len(nums) - k:
+                right = pivot_idx - 1
+            else:
+                left = pivot_idx + 1
 
-            if new_pivot_idx == k - 1:
-                return nums[new_pivot_idx]
-            elif new_pivot_idx > k - 1:
-                right = new_pivot_idx - 1
-            else:  # new_pivot_idx < k - 1.
-                left = new_pivot_idx + 1
+    def partition(self, nums, start_index, end_index):
+        pivot = nums[end_index]
 
-    def partition_around_pivot(self, left, right, pivot_idx, nums):
-        pivot_value = nums[pivot_idx]
-        new_pivot_idx = left
-        nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
-        for i in range(left, right + 1):
-            if nums[i] > pivot_value:
-                nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
-                new_pivot_idx += 1
+        pivot_index = start_index - 1
+        for num_index in range(start_index, end_index):
+            if nums[num_index] <= pivot:
+                pivot_index += 1
+                nums[pivot_index], nums[num_index] = nums[num_index], nums[pivot_index]
 
-        nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
-        return new_pivot_idx
+        pivot_index += 1
+        nums[pivot_index], nums[end_index] = nums[end_index], nums[pivot_index]
+        return pivot_index
