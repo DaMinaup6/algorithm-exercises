a9be03a0b4695a97a8177c40c534c61189329f98
DaMinaup6 <danaup6@gmail.com>
2021-06-04 17:20:05 +0800

    algorithm: update sliding window maximum problem

--- leetcode/hard/239_sliding_window_maximum.py
+++ leetcode/hard/239_sliding_window_maximum.py
@@ -7,19 +7,21 @@
 # Ref:
 # a) https://blog.csdn.net/fuxuemingzhu/article/details/100828798
 # b) https://www.youtube.com/watch?v=2SXqBsTR6a8
+# c) https://www.educative.io/edpresso/tuples-vs-list-in-python
 from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        queue  = deque() # [[index, num]]
+        # store tuple would use less memory and the speed of creation is faster. Ref: https://www.educative.io/edpresso/tuples-vs-list-in-python
+        queue  = deque() # [(index, num)]
         output = []
 
         for index, num in enumerate(nums):
-            if queue and index - queue[0][0] >= k:
+            if len(queue) > 0 and index - queue[0][0] == k:
                 queue.popleft()
-            while queue and queue[-1][1] <= num:
+            while len(queue) > 0 and queue[-1][1] <= num:
                 queue.pop()
-            queue.append([index, num])
+            queue.append((index, num))
 
             if index >= k - 1:
                 output.append(queue[0][1])
