f3bdf00d5fc04d7feb8614d1776bb6d6f395938a
DaMinaup6 <danaup6@gmail.com>
2021-06-04 21:49:17 +0800

    algorithm: update frog jump problem

--- leetcode/hard/403_frog_jump.py
+++ leetcode/hard/403_frog_jump.py
@@ -10,28 +10,21 @@ class Solution:
     def canCross(self, stones: List[int]) -> bool:
         if stones[1] > 1:
             return False
-        if len(stones) == 2:
-            return True
 
-        stones_position_index_map = {}
-        for index, stone_position in enumerate(stones):
-            stones_position_index_map[stone_position] = index
+        positions = set(stones)
+        last_position = max(stones)
 
         @lru_cache(None)
-        def dfs(curr_stone_index, prev_jump_unit):
-            if curr_stone_index == len(stones) - 1:
+        def can_cross_from(curr_position, last_jump):
+            if curr_position == last_position:
                 return True
 
-            curr_position = stones[curr_stone_index]
-            for next_jump_unit in [prev_jump_unit - 1, prev_jump_unit, prev_jump_unit + 1]:
-                if next_jump_unit <= 0 or curr_position + next_jump_unit not in stones_position_index_map:
-                    continue
-                next_stone_index = stones_position_index_map[curr_position + next_jump_unit]
-                if dfs(next_stone_index, next_jump_unit):
+            for next_jump in (last_jump - 1, last_jump, last_jump + 1):
+                if next_jump > 0 and curr_position + next_jump in positions and can_cross_from(curr_position + next_jump, next_jump):
                     return True
             return False
 
-        return dfs(1, 1)
+        return can_cross_from(1, 1)
 
 # -----------------------------------------
 # My Solution: Dynamic Programming
@@ -43,16 +36,14 @@ class Solution:
     def canCross(self, stones: List[int]) -> bool:
         if stones[1] > 1:
             return False
-        if len(stones) == 2:
-            return True
 
         stones_position_index_map = {}
         for index, stone_position in enumerate(stones):
             stones_position_index_map[stone_position] = index
 
         prev_jump_units = [set() for _ in stones]
-        prev_jump_units[0], prev_jump_units[1] = set([0]), set([1])
-        for index in range(1, len(stones)):
+        prev_jump_units[0] = set([0])
+        for index in range(len(stones)):
             curr_position = stones[index]
             for prev_jump_unit in prev_jump_units[index]:
                 for next_jump_unit in [prev_jump_unit - 1, prev_jump_unit, prev_jump_unit + 1]:
