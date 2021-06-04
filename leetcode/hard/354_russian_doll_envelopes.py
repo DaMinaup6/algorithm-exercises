e41a66bb97c040b468caf3fe62b29b587f2d7716
DaMinaup6 <danaup6@gmail.com>
2021-06-04 21:01:13 +0800

    algorithm: update russian doll envelopes problem

--- leetcode/hard/354_russian_doll_envelopes.py
+++ leetcode/hard/354_russian_doll_envelopes.py
@@ -28,13 +28,20 @@ import bisect
 
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        # since one envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height,
+        # i.e. [1, 2] can fit into [3, 4] but cannot fit into [1, 3]
+        # so we need to sort envelopes by (width, -height)
+        # e.g. envelopes == [[1, 1], [1, 2], [1, 3]]
+        #      obviously the answer is 1, but if just do envelopes.sort() and apply patience sort then we get answer 3, which is wrong
+        #      so after envelopes.sort(key=lambda envelope: (envelope[0], -envelope[1])) we get envelopes == [[1, 3], [1, 2], [1, 1]]
+        #      this time patience sort would give us correct answer
         envelopes.sort(key=lambda envelope: (envelope[0], -envelope[1]))
         patience_sort_list = []
         for _, height in envelopes:
-            prev_index = bisect.bisect_left(patience_sort_list, height)
-            if prev_index == len(patience_sort_list):
+            height_index = bisect.bisect_left(patience_sort_list, height)
+            if height_index == len(patience_sort_list):
                 patience_sort_list.append(height)
             else:
-                patience_sort_list[prev_index] = height
+                patience_sort_list[height_index] = height
 
         return len(patience_sort_list)
