b5ddc6243829e2462718d1ed3565209e9ca683af
DaMinaup6 <danaup6@gmail.com>
2021-06-05 18:07:36 +0800

    algorithm: update redundant connection problem

--- leetcode/medium/684_redundant_connection.py
+++ leetcode/medium/684_redundant_connection.py
@@ -12,7 +12,7 @@ class Solution:
             if node != union_root[node]:
                 union_root[node] = find_union_root(union_root[union_root[node]])
             return union_root[node]
-        
+
         for edge in edges:
             # from 1-indexed to 0-indexed
             node_1 = edge[0] - 1
@@ -24,3 +24,41 @@ class Solution:
             union_root[node_1_root] = node_2_root
 
         return [-1, -1]
+
+# -----------------------------------------
+# Model Solution: Union Find
+#
+# Time  Complexity: O(nlog(n))
+# Space Complexity: O(n)
+# -----------------------------------------
+# Ref: https://www.youtube.com/watch?v=FXWRE67PLL0
+class Solution:
+    def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
+        parents = [node for node in range(len(edges) + 1)]
+        ranks   = [1] * (len(edges) + 1)
+
+        def find_parent(node):
+            parent = parents[node]
+            while parent != parents[parent]:
+                parents[parent] = parents[parents[parent]]
+                parent = parents[parent]
+            return parent
+
+        # return False if node_1 and node_2 share same parent, means they already connected
+        def union_nodes(node_1, node_2):
+            parent_1, parent_2 = find_parent(node_1), find_parent(node_2)
+            if parent_1 == parent_2:
+                return False
+
+            if ranks[parent_1] > ranks[parent_2]:
+                parents[parent_2] = parent_1
+                ranks[parent_1] += ranks[parent_2]
+            else:
+                parents[parent_1] = parent_2
+                ranks[parent_2] += ranks[parent_1]
+            return True
+
+        for node_1, node_2 in edges:
+            if not union_nodes(node_1, node_2):
+                return [node_1, node_2]
+        return []
