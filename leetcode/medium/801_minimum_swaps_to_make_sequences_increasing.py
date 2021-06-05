59b18d3e47265ca102618a3aa54e23062c7f3ef2
DaMinaup6 <danaup6@gmail.com>
2021-06-05 18:43:42 +0800

    algorithm: update minimum swaps to make sequences increasing problem

--- leetcode/medium/801_minimum_swaps_to_make_sequences_increasing.py
+++ leetcode/medium/801_minimum_swaps_to_make_sequences_increasing.py
@@ -11,6 +11,9 @@ class Solution:
         for index in range(1, len(nums1)):
             next_keep, next_swap = float('inf'), float('inf')
             if nums1[index] > nums1[index - 1] and nums2[index] > nums2[index - 1]:
+                # here +1 for next_swap means we need to swap previous elements to keep the array increasing
+                # e.g. nums1 == [1, 2], nums2 == [1, 3]
+                #      if we want to do swap for nums1[1] and nums2[1], then we need to swap nums1[0] and nums2[0] also to keep it increasing
                 next_keep = curr_keep
                 next_swap = curr_swap + 1
             if nums1[index] > nums2[index - 1] and nums2[index] > nums1[index - 1]:
